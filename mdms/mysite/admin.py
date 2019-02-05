edb032bda80b321684e719c5d0f3542b1eae9a43
BrunoFromMars <jayeshgiramkars911@gmail.com>
2019-02-05 13:17:26 +0530

    MDMS site ,first commit

---
+++ mdms/mysite/admin.py
@@ -0,0 +1,7 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from mysite.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
